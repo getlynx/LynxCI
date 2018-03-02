753448de70e7247e0f574e8f78a0086d373ee471
Ben <ben@getlynx.io>
2018-03-02 00:13:19 -0500

    Removed Mongo install. Will do later.

--- buildTest.sh
+++ buildTest.sh
@@ -373,43 +373,6 @@ install_cpuminer () {
 
 }
 
-
-install_mongo () {
-
-	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-	echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-	apt-get update
-	apt-get install -y mongodb-org
-
-	echo "
-
-		[Unit]
-		Description=High-performance, schema-free document-oriented database
-		After=network.target
-		Documentation=https://docs.mongodb.org/manual
-
-		[Service]
-		User=mongodb
-		Group=mongodb
-		ExecStart=/usr/bin/mongod --quiet --config /etc/mongod.conf
-
-		[Install]
-		WantedBy=multi-user.target
-
-	" > /lib/systemd/system/mongod.service
-
-	systemctl daemon-reload
-	systemctl start mongod
-	systemctl enable mongod
-
-	service mongod start
-	mongo
-	use explorerdb
-	db.createUser( { user: "$rrpcuser", pwd: "$rrpcpassword", roles: [ "readWrite" ] } )
-	exit
-
-}
-
 set_firewall () {
 
 	rm -rf /root/firewall.sh
