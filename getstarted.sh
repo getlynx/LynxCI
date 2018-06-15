857481aaf684b860f309dad656113f624790a819
Ben <ben@getlynx.io>
2018-06-15 00:22:14 -0400

    Refactoring

--- getstarted.sh
+++ getstarted.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get update -y && apt-get install git -y
+apt-get update -y &> /dev/null && apt-get install git -y &> /dev/null
 git clone https://github.com/doh9Xiet7weesh9va9th/LynxNodeBuilder.git /root/LynxNodeBuilder/
 chmod 744 -R /root/LynxNodeBuilder/
 /root/LynxNodeBuilder/build.sh
