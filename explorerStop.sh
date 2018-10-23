0bbc333c045ef7597712b1f66ec6b0d634695961
Ben <ben@getlynx.io>
2018-10-22 21:35:01 -0400

    Keep the beginning of the debog intact.
    We don't truncate the whole .lynx/debug.log so we can always tell how old the node is and what block it started on.

--- explorerStop.sh
+++ explorerStop.sh
@@ -63,7 +63,7 @@ crontab -l | { cat; echo "0 0 $(shuf -i 1-15 -n 1) * *		/root/LynxCI/update.sh";
 # We found that after a few weeks, the debug log would grow rather large. It's not really needed
 # after a certain size, so let's truncate that log down to a reasonable size every day.
 
-crontab -l | { cat; echo "*/30 * * * *		truncate -s 1KB /root/.lynx/debug.log"; } | crontab -
+crontab -l | { cat; echo "*/30 * * * *		truncate -s 5KB /root/.lynx/debug.log"; } | crontab -
 
 # Evey 15 days we will reboot the device. This is for a few reasons. Since the device is often
 # not actively managed by it's owner, we can't assume it is always running perfectly so an
