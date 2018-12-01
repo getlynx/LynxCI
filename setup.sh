b94bf6e86bd5749db676e3993b9faaea843ebdc6
Ben <ben@getlynx.io>
2018-12-01 18:15:22 -0500

    Adding spaces around the printf outputs.

--- setup.sh
+++ setup.sh
@@ -33,7 +33,7 @@ printf "Unlike Bitcoin, the Lynx code discourages high-volume mining rigs becaus
 
 printf "Lynx is 'CRYPTOCURRENCY WITHOUT THE CLIMATE CHANGE'\n\n\n"
 
-printf "Assembling the latest code to install LynxCI.\n\n"
+printf "Assembling the latest code to install LynxCI.\n\n\n\n\n\n"
 
 if [ -z "$checkForRaspbian" ]; then
 
