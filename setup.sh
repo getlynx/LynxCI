75cb2593a7f4e05d8442d511f391e6188850eb46
Ben <ben@getlynx.io>
2019-01-31 10:16:23 -0500

    suppressing some outputs.

--- setup.sh
+++ setup.sh
@@ -51,23 +51,23 @@ fi
 # Before we begin, we need to update the local repo's. For now, the update is all we need and the
 # device will still function properly.
 
-apt-get -qq update -y
+apt-get -qq update -y &> /dev/null
 
 # Some hosting vendors already have these installed. They aren't needed, so we are removing them
 # now. This list will probably get longer over time.
 
-apt-get -qq remove -y postfix apache2
+apt-get -qq remove -y postfix apache2 &> /dev/null
 
 # Now that certain packages that might bring an interactive prompt are removed, let's do an upgrade.
 
-apt-get -qq upgrade -y
+apt-get -qq upgrade -y &> /dev/null
 
 # We need to ensure we have git for the following step. Let's not assume we already ahve it. Also
 # added a few other tools as testing has revealed that some vendors didn't have them pre-installed.
 
-apt-get -qq install -y git git-core htop nano
+apt-get -qq install -y git git-core htop nano &> /dev/null
 
-apt-get -qq autoremove -y
+apt-get -qq autoremove -y &> /dev/null
 
 # Lets not assume this is the first time the script has been attempted.
 
