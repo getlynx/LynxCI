3296efbc4c61ebceeb1bba1ce8bb454e7924703b
Ben <ben@getlynx.io>
2019-02-07 14:23:07 -0500

    adding more libs

--- compilers/Ubuntu-16.04.sh
+++ compilers/Ubuntu-16.04.sh
@@ -8,17 +8,13 @@ GIT_DIR="/root/lynx"
 #SSL_VERSION="1.0.0" # <-- DEBIAN
 SSL_VERSION="" # <-- UBUNTU (blank)
 LIBRARIES="libssl${SSL_VERSION}-dev libboost-all-dev libevent-dev libminiupnpc-dev"
-DEV_TOOLS="build-essential libtool autotools-dev autoconf cmake pkg-config bsdmainutils git"
+DEV_TOOLS="build-essential libtool autotools-dev autoconf cmake pkg-config bsdmainutils git wget"
 
 # Ubuntu 16.04 Requirement
 add-apt-repository -y ppa:bitcoin/bitcoin
 apt-get -qq update -y
 apt-get -qq install -y libdb4.8-dev
 apt-get -qq install -y libdb4.8++-dev
-
-# Install Boostrap First
-mkdir ${CONF_DIR}
-wget https://github.com/getlynx/Lynx/releases/download/v0.16.3.5/bootstrap.tar.gz -O - | tar -xz -C ${CONF_DIR}
 
 touch ${CONF_FILE}
 
@@ -38,7 +34,17 @@ rm -Rf /root/lynxd/DEBIAN/postinst*
 rm -Rf /root/lynxd_16.3.5-1_amd64.deb
 mkdir lynxd && mkdir lynxd/DEBIAN
 mkdir -p lynxd/usr/local/bin/
+
+# Lets place all the files and dependencies we need into the package.
+
 cp /usr/local/bin/lynx* lynxd/usr/local/bin/
+cp /usr/lib/x86_64-linux-gnu/libboost_system.so.1.58.0 lynxd/usr/lib/x86_64-linux-gnu/libboost_system.so.1.58.0
+cp /usr/lib/x86_64-linux-gnu/libboost_filesystem.so.1.58.0 lynxd/usr/lib/x86_64-linux-gnu/libboost_filesystem.so.1.58.0
+cp /usr/lib/x86_64-linux-gnu/libboost_program_options.so.1.58.0 lynxd/usr/lib/x86_64-linux-gnu/libboost_program_options.so.1.58.0
+cp /usr/lib/x86_64-linux-gnu/libboost_thread.so.1.58.0 lynxd/usr/lib/x86_64-linux-gnu/libboost_thread.so.1.58.0
+cp /usr/lib/x86_64-linux-gnu/libboost_chrono.so.1.58.0 lynxd/usr/lib/x86_64-linux-gnu/libboost_chrono.so.1.58.0
+cp /usr/lib/x86_64-linux-gnu/libssl.so lynxd/usr/lib/x86_64-linux-gnu/libssl.so
+cp /usr/lib/x86_64-linux-gnu/libcrypto.so lynxd/usr/lib/x86_64-linux-gnu/libcrypto.so
 
 echo "
 
@@ -50,7 +56,7 @@ Description: https://getlynx.io
 
 " > /root/lynxd/DEBIAN/control
 
-#wget http://cdn.getlynx.io/postinst ‐P /lynxd/DEBIAN 
+wget https://raw.githubusercontent.com/getlynx/LynxCI/master/compilers/postinst ‐P /lynxd/DEBIAN 
 
 chmod -R 755 /root/lynxd/*
 
@@ -58,11 +64,6 @@ cd /root/ && dpkg-deb --build lynxd
 
 mv lynxd.deb lynxd_16.3.5-1_amd64.deb
 
-# curl -sLO http://cdn.getlynx.io/lynxd_16.3.5-1_amd64.deb && dpkg -i lynxd_16.3.5-1_amd64.deb
-
+#curl -sLO http://cdn.getlynx.io/lynxd_16.3.5-1_amd64.deb && dpkg -i lynxd_16.3.5-1_amd64.deb
 #curl -sLO http://cdn.getlynx.io/lynxd.deb && dpkg -i lynxd.deb
-
-
-
-
 
