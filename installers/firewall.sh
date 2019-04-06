321995f9f7c1c044fc90fcbbb13e711e6150f714
Ben <ben@getlynx.io>
2019-04-06 11:42:39 -0400

    fixed rule order

--- installers/firewall.sh
+++ installers/firewall.sh
@@ -28,6 +28,50 @@ fi
 
 /sbin/iptables -I INPUT 2 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
+# Becuase the Block Explorer or Block Crawler are available via port 80 (standard website port)
+# we must open up port 80 for that traffic.
+
+/sbin/iptables -A INPUT -p tcp --dport 80 -j DROP
+
+# Known addresses of other coin projects that operate on the same port and have the same version
+# number. This will remove a good number of 'connection refused' errors in the debug log.
+
+/sbin/iptables -A INPUT -p tcp -s 192.210.200.58 -j DROP #Known out of date node
+/sbin/iptables -A INPUT -p tcp -s 193.29.56.183 -j DROP #Known out of date node
+/sbin/iptables -A INPUT -p tcp -s 31.204.147.56 -j DROP #Known out of date node
+/sbin/iptables -A INPUT -p tcp -s 50.225.198.67 -j DROP #Known out of date node
+/sbin/iptables -A INPUT -p tcp -s 185.175.56.132 -j DROP #Known out of date node
+/sbin/iptables -A INPUT -p tcp -s 82.199.45.7 -j DROP #Known out of date node
+/sbin/iptables -A INPUT -p tcp -s 118.240.210.46 -j DROP #ExperiencecoinCore:3.0.0.1
+/sbin/iptables -A INPUT -p tcp -s 146.120.14.160 -j DROP #ExperiencecoinCore:3.0.0.1
+/sbin/iptables -A INPUT -p tcp -s 159.203.134.242 -j DROP #ExperiencecoinCore:3.0.0.1
+/sbin/iptables -A INPUT -p tcp -s 159.65.189.70 -j DROP #Satoshi:0.15.0.1
+/sbin/iptables -A INPUT -p tcp -s 165.227.211.179 -j DROP #ExperiencecoinCore:3.0.0.1
+/sbin/iptables -A INPUT -p tcp -s 178.118.21.128 -j DROP #NewYorkCoin-seeder:0.01
+/sbin/iptables -A INPUT -p tcp -s 178.62.59.145 -j DROP #ExperiencecoinCore:3.0.0.1
+/sbin/iptables -A INPUT -p tcp -s 188.166.62.180 -j DROP #/blockchair.com/dogecoin/nodes/
+/sbin/iptables -A INPUT -p tcp -s 2.226.152.231 -j DROP #ExperiencecoinCore:3.0.0.1
+/sbin/iptables -A INPUT -p tcp -s 200.252.9.194 -j DROP #ExperiencecoinCore:3.0.0.1
+/sbin/iptables -A INPUT -p tcp -s 207.154.242.254 -j DROP #ExperiencecoinCore:3.0.0.1
+/sbin/iptables -A INPUT -p tcp -s 50.225.198.67 -j DROP #ExperiencecoinCore:3.0.0.1
+/sbin/iptables -A INPUT -p tcp -s 62.213.218.8 -j DROP #NewYorkCoin-seeder:0.01
+/sbin/iptables -A INPUT -p tcp -s 73.164.61.211 -j DROP #ExperiencecoinCore:3.0.0.1
+/sbin/iptables -A INPUT -p tcp -s 74.124.24.246 -j DROP #ExperiencecoinCore:3.0.0.1
+/sbin/iptables -A INPUT -p tcp -s 75.88.232.28 -j DROP #ExperiencecoinCore:3.0.0.1
+/sbin/iptables -A INPUT -p tcp -s 76.102.131.12 -j DROP #NewYorkCoin-seeder:0.01
+/sbin/iptables -A INPUT -p tcp -s 78.81.182.206 -j DROP #ExperiencecoinCore:3.0.0.1
+/sbin/iptables -A INPUT -p tcp -s 80.82.49.16 -j DROP #ExperiencecoinCore:3.0.0.1
+/sbin/iptables -A INPUT -p tcp -s 85.236.188.28 -j DROP #Satoshi:0.8.6.2
+/sbin/iptables -A INPUT -p tcp -s 88.99.91.47 -j DROP #CryptoTransfer:1.0.0
+/sbin/iptables -A INPUT -p tcp -s 94.130.16.85 -j DROP #ExperiencecoinCore:3.0.0.1
+/sbin/iptables -A INPUT -p tcp -s 94.177.201.91 -j DROP #ExperiencecoinCore:3.0.0.1
+/sbin/iptables -A INPUT -p tcp -s 95.52.43.220 -j DROP #ExperiencecoinCore:3.0.0.1
+/sbin/iptables -A INPUT -p tcp -s 95.52.42.249 -j DROP #ExperiencecoinCore:3.0.0.1
+/sbin/iptables -A INPUT -p tcp -s 95.54.68.250 -j DROP #ExperiencecoinCore:3.0.0.1
+/sbin/iptables -A INPUT -p tcp -s 95.54.69.24 -j DROP #ExperiencecoinCore:3.0.0.1
+/sbin/iptables -A INPUT -p tcp -s 95.68.166.255 -j DROP #ExperiencecoinCore:3.0.0.1
+/sbin/iptables -A INPUT -p tcp -s 95.68.196.178 -j DROP #ExperiencecoinCore:3.0.0.1
+
 # If the script has IsRestricted set to Y, then let's open up port 22 for any IP address. But if
 # the script has IsRestricted set to N, let's only open up port 22 for local LAN access. This means
 # you have to be physically connected (or via Wifi) to SSH to this computer. It isn't perfectly
@@ -86,54 +130,10 @@ else
 
 fi
 
-# Becuase the Block Explorer or Block Crawler are available via port 80 (standard website port)
-# we must open up port 80 for that traffic.
-
-/sbin/iptables -A INPUT -p tcp --dport 80 -j DROP
-
 # This Lynx node listens for other Lynx nodes on port $port, so we need to open that port. The
 # whole Lynx network listens on that port so we always want to make sure this port is available.
 
 /sbin/iptables -A INPUT -p tcp --dport _port_ -j ACCEPT
-
-# Known addresses of other coin projects that operate on the same port and have the same version
-# number. This will remove a good number of 'connection refused' errors in the debug log.
-
-/sbin/iptables -A INPUT -p tcp -s 192.210.200.58 -j DROP
-/sbin/iptables -A INPUT -p tcp -s 193.29.56.183 -j DROP
-/sbin/iptables -A INPUT -p tcp -s 31.204.147.56 -j DROP
-/sbin/iptables -A INPUT -p tcp -s 50.225.198.67 -j DROP
-/sbin/iptables -A INPUT -p tcp -s 185.175.56.132 -j DROP
-/sbin/iptables -A INPUT -p tcp -s 82.199.45.7 -j DROP
-/sbin/iptables -A INPUT -p tcp -s 118.240.210.46 -j DROP #ExperiencecoinCore:3.0.0.1
-/sbin/iptables -A INPUT -p tcp -s 146.120.14.160 -j DROP #ExperiencecoinCore:3.0.0.1
-/sbin/iptables -A INPUT -p tcp -s 159.203.134.242 -j DROP #ExperiencecoinCore:3.0.0.1
-/sbin/iptables -A INPUT -p tcp -s 159.65.189.70 -j DROP #Satoshi:0.15.0.1
-/sbin/iptables -A INPUT -p tcp -s 165.227.211.179 -j DROP #ExperiencecoinCore:3.0.0.1
-/sbin/iptables -A INPUT -p tcp -s 178.118.21.128 -j DROP #NewYorkCoin-seeder:0.01
-/sbin/iptables -A INPUT -p tcp -s 178.62.59.145 -j DROP #ExperiencecoinCore:3.0.0.1
-/sbin/iptables -A INPUT -p tcp -s 188.166.62.180 -j DROP #/blockchair.com/dogecoin/nodes/
-/sbin/iptables -A INPUT -p tcp -s 2.226.152.231 -j DROP #ExperiencecoinCore:3.0.0.1
-/sbin/iptables -A INPUT -p tcp -s 200.252.9.194 -j DROP #ExperiencecoinCore:3.0.0.1
-/sbin/iptables -A INPUT -p tcp -s 207.154.242.254 -j DROP #ExperiencecoinCore:3.0.0.1
-/sbin/iptables -A INPUT -p tcp -s 50.225.198.67 -j DROP #ExperiencecoinCore:3.0.0.1
-/sbin/iptables -A INPUT -p tcp -s 62.213.218.8 -j DROP #NewYorkCoin-seeder:0.01
-/sbin/iptables -A INPUT -p tcp -s 73.164.61.211 -j DROP #ExperiencecoinCore:3.0.0.1
-/sbin/iptables -A INPUT -p tcp -s 74.124.24.246 -j DROP #ExperiencecoinCore:3.0.0.1
-/sbin/iptables -A INPUT -p tcp -s 75.88.232.28 -j DROP #ExperiencecoinCore:3.0.0.1
-/sbin/iptables -A INPUT -p tcp -s 76.102.131.12 -j DROP #NewYorkCoin-seeder:0.01
-/sbin/iptables -A INPUT -p tcp -s 78.81.182.206 -j DROP #ExperiencecoinCore:3.0.0.1
-/sbin/iptables -A INPUT -p tcp -s 80.82.49.16 -j DROP #ExperiencecoinCore:3.0.0.1
-/sbin/iptables -A INPUT -p tcp -s 85.236.188.28 -j DROP #Satoshi:0.8.6.2
-/sbin/iptables -A INPUT -p tcp -s 88.99.91.47 -j DROP #CryptoTransfer:1.0.0
-/sbin/iptables -A INPUT -p tcp -s 94.130.16.85 -j DROP #ExperiencecoinCore:3.0.0.1
-/sbin/iptables -A INPUT -p tcp -s 94.177.201.91 -j DROP #ExperiencecoinCore:3.0.0.1
-/sbin/iptables -A INPUT -p tcp -s 95.52.43.220 -j DROP #ExperiencecoinCore:3.0.0.1
-/sbin/iptables -A INPUT -p tcp -s 95.52.42.249 -j DROP #ExperiencecoinCore:3.0.0.1
-/sbin/iptables -A INPUT -p tcp -s 95.54.68.250 -j DROP #ExperiencecoinCore:3.0.0.1
-/sbin/iptables -A INPUT -p tcp -s 95.54.69.24 -j DROP #ExperiencecoinCore:3.0.0.1
-/sbin/iptables -A INPUT -p tcp -s 95.68.166.255 -j DROP #ExperiencecoinCore:3.0.0.1
-/sbin/iptables -A INPUT -p tcp -s 95.68.196.178 -j DROP #ExperiencecoinCore:3.0.0.1
 
 # By default, the RPC port 9223 is opened to the public. This is so the node can both listen
 # for and discover other nodes. It is preferred to have a node that is not just a leecher but
