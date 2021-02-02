cf66a9b058cdf503e0aa41ff92e60dbe02be8836
Benjamin Wilson <b@getlynx.io>
2021-02-01 21:25:22 -0500

    for initialization on a the Pi ISO

--- init.sh
+++ init.sh
@@ -8,58 +8,12 @@ sleep 60
 
 # Ping Google NS server to test public network access
 
-if /bin/ping -c 1 8.8.8.8 &> /dev/null
+if /bin/ping -c 1 8.8.8.8
 then
 
-	# Remove the file if it already exists from a previous try.
+	sleep 30
 
-	rm -rf /root/setup.sh
-
-	# Grab the remote file. If it failes for any reason, reboot.
-
-	if /usr/bin/wget -O /root/setup.sh https://getlynx.io/setup.sh; then
-
-		# File exists and is a regular file. A regular file is neither a block or character special file nor a directory.
-
-		if [ -f "/root/setup.sh" ]; then
-
-			# File exists and has a size of more than 0 bytes.
-
-			if [ -s "/root/setup.sh" ]; then
-
-				/bin/chmod 700 /root/setup.sh
-
-				/root/setup.sh
-
-			else
-
-				echo "Downloaded setup script was corrupted (-s). Rebooting and will try again."
-
-				sleep 30
-
-				reboot
-
-			fi
-
-		else
-
-			echo "Downloaded setup script was corrupted (-f). Rebooting and will try again."
-
-			sleep 30
-
-			reboot
-
-		fi
-
-	else
-
-		echo "Failed to download setup script. Rebooting and will try again."
-
-		sleep 30
-
-		reboot
-
-	fi
+	wget -qO - https://getlynx.io/install.sh | bash
 
 else
 
