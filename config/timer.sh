9880a06a41e4cc5e975c1e61bebfd331eec60da4
Benjamin Wilson <b@getlynx.io>
2021-10-21 00:23:30 -0400

    test round

--- config/timer.sh
+++ config/timer.sh
@@ -7,7 +7,6 @@ systemctl daemon-reload
 echo "
 [Unit]
 Description=Update LynxCI
-
 [Service]
 Type=oneshot
 ExecStart=/usr/bin/wget -qO - https://raw.githubusercontent.com/getlynx/LynxCI/master/config/update.sh | bash
@@ -16,15 +15,12 @@ ExecStart=/usr/bin/wget -qO - https://raw.githubusercontent.com/getlynx/LynxCI/m
 chmod 644 /etc/systemd/system/update.service
 chown root:root /etc/systemd/system/update.service
 
-
 echo "
 [Unit]
 Description=Update LynxCI on boot
-
 [Timer]
 OnBootSec=5min
 Unit=update.service
-
 [Install]
 WantedBy=multi-user.target
 " > /etc/systemd/system/update.timer # Create the systemd timer unit
