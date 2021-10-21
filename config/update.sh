e9cdd6d6255188212ba6f60c37828bfc12054c2a
Benjamin Wilson <b@getlynx.io>
2021-10-20 23:03:25 -0400

    Update update.sh

--- config/update.sh
+++ config/update.sh
@@ -13,10 +13,10 @@ fi
 
 # Update the Message of the Day display content
 file="https://raw.githubusercontent.com/getlynx/LynxCI/master/config/motd.sh"
-local=$(md5sum /usr/local/bin/config/motd.sh | head -c 32)
+local=$(md5sum /etc/profile.d/motd.sh | head -c 32)
 remote=$(wget -O - $file | md5sum | head -c 32)
 
 if [ "$local" != "$remote" ]; then 
-	wget $file -O motd.sh -P /etc/profile.d/ -P /usr/local/bin/config/ | bash
+	wget $file -O motd.sh -P /etc/profile.d/ | bash
 fi
 
