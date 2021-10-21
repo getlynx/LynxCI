7ffadd0325b1872f7c69be617c288e8cf432a626
Benjamin Wilson <b@getlynx.io>
2021-10-20 23:53:29 -0400

    -q on wget

--- config/update.sh
+++ config/update.sh
@@ -7,10 +7,9 @@
 ##################################################################################################################
 file="https://raw.githubusercontent.com/getlynx/LynxCI/master/config/timer.sh"
 local=$(md5sum /usr/local/bin/config/timer.sh | head -c 32)
-remote=$(wget -O - $file | md5sum | head -c 32)
-
+remote=$(wget -qO - $file | md5sum | head -c 32)
 if [ "$local" != "$remote" ]; then 
-	wget -O - $file > /usr/local/bin/config/timer.sh
+	wget -qO - $file > /usr/local/bin/config/timer.sh
 	chmod 744 /usr/local/bin/config/timer.sh
 	/usr/local/bin/config/timer.sh
 fi
@@ -19,9 +18,8 @@ fi
 ##################################################################################################################
 file="https://raw.githubusercontent.com/getlynx/LynxCI/master/config/motd.sh"
 local=$(md5sum /etc/profile.d/motd.sh | head -c 32)
-remote=$(wget -O - $file | md5sum | head -c 32)
-
+remote=$(wget -qO - $file | md5sum | head -c 32)
 if [ "$local" != "$remote" ]; then 
-	wget -O - $file > /etc/profile.d/motd.sh
+	wget -qO - $file > /etc/profile.d/motd.sh
 fi
 ##################################################################################################################
