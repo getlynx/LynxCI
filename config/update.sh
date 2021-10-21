ef4dbc4edc2456d7030b607c9dece78483f68bba
Benjamin Wilson <b@getlynx.io>
2021-10-21 18:17:42 -0400

    Update update.sh

--- config/update.sh
+++ config/update.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 ##################################################################################################################
-# wget -qO - https://raw.githubusercontent.com/getlynx/LynxCI/master/config/update.sh | bash
-# wget -O - https://raw.githubusercontent.com/getlynx/LynxCI/master/config/update.sh > /usr/local/bin/config/update.sh && /usr/local/bin/config/update.sh
+# Install with: $ wget -qO - https://raw.githubusercontent.com/getlynx/LynxCI/master/config/update.sh | bash
 [ $EUID -ne 0 ] && echo "This script must be run from the root account. Exiting." && exit
 mkdir -p /usr/local/bin/config/
 wget -O - https://raw.githubusercontent.com/getlynx/LynxCI/master/config/update.sh > /usr/local/bin/config/update.sh
