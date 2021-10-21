46b027efeefb953fb34bfe1fdac521bf498de23a
Benjamin Wilson <b@getlynx.io>
2021-10-20 22:49:04 -0400

    Update update.sh

--- config/update.sh
+++ config/update.sh
@@ -2,7 +2,7 @@
 
 # wget -O - https://raw.githubusercontent.com/getlynx/LynxCI/master/config/update.sh | bash
 
-# Update the Message of the Day display content
+# Update the Timer content
 file="https://raw.githubusercontent.com/getlynx/LynxCI/master/config/timer.sh"
 local=$(md5sum /usr/local/bin/config/timer.sh | head -c 32)
 remote=$(wget -O - $file | md5sum | head -c 32)
@@ -17,6 +17,6 @@ local=$(md5sum /usr/local/bin/config/motd.sh | head -c 32)
 remote=$(wget -O - $file | md5sum | head -c 32)
 
 if [ "$local" != "$remote" ]; then 
-	wget - $file -O motd.sh | bash
+	wget - $file -O motd.sh -P /etc/profile.d/ | bash
 fi
 
