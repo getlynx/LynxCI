77d951d4320d9c79c5c0ee7df097a63b1133cf0e
Benjamin Wilson <b@getlynx.io>
2021-10-24 11:44:17 -0400

    Update updateTest.sh

--- config/updateTest.sh
+++ config/updateTest.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+echo "update.service: Run the date command." | systemd-cat -p info
 date
 
 
 
 
 
-
-rm -rf /usr/local/bin/config/updateTest.*
+echo "update.service: Run the remove command." | systemd-cat -p info
+rm -rf /usr/local/bin/config/updateTest.sh
