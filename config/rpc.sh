5081d6c7e2beff098ab36ca88fbb9b35818f41bb
Lynx Blockchain Development <38914109+getlynx@users.noreply.github.com>
2022-05-10 21:30:28 -0400

    Create rpc.sh

---
+++ config/rpc.sh
@@ -0,0 +1,6 @@
+echo "rpcuser=$(date +%N | sha256sum | awk '{print $1}')
+rpcpassword=$(date +%N | sha256sum | awk '{print $1}')
+rpcport=9332
+rpcallowip=0.0.0.0/24
+rpcallowip=::/0
+rpcworkqueue=64"
